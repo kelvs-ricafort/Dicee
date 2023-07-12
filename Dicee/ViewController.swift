6cbcd9ca0e02e8d83aedddb178f3125a745fcd6b
Kelvin C. Ricafort <kelvs.r@icloud.com>
2023-07-12 13:16:11 +0800

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Kelvin Ricafort on 7/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
