2c9733aa6a36a8bc0bdfe4b80163d1877f09b0c8
Kelvin C. Ricafort <kelvs.r@icloud.com>
2023-07-13 14:58:05 +0800

    With AutoLayout

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -21,7 +21,6 @@ class ViewController: UIViewController {
    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        // 
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
         diceImageView1.image = diceArray.randomElement()
